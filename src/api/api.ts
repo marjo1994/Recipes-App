d2aa20de969fde4559c44496eb6f191656cc3b58
marjo1994 <m_rhl@live.com>
2025-08-18 13:01:42 -0500

    feat(apiKey) : converting apiKey dynamic from Vercel

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,11 +1,12 @@
 import type { RecipeFilters } from '../types';
 
+const apiKey = import.meta.env.VITE_API_KEY;
+
 //const API_KEY = '396599591c3c4d2cb2ed85ffa0abce06';
-const API_KEY = '62bf9fa758b749009ded451fa07c163b';
 const BASE_URL = 'https://api.spoonacular.com/recipes';
 
 export const searchRecipes = async(query: string) => {
-    const response = await fetch(`${BASE_URL}/complexSearch?apiKey=${API_KEY}&query=${query}&number=5`)
+    const response = await fetch(`${BASE_URL}/complexSearch?apiKey=${apiKey}&query=${query}&number=5`)
 
     if(!response.ok) {
         throw new Error('Failed to fetch recipes')
@@ -20,7 +21,7 @@ export const fetchRecipes = async(filters: RecipeFilters = {}) => {
   const { mealTypes = [], includeIngredients = [] } = filters;
 
     const queryParams = new URLSearchParams({
-        apiKey: API_KEY,
+        apiKey,
         addRecipeInformation: 'true',
         number: '9',
         ...(includeIngredients.length > 0 && { 
