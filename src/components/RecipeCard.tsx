16d69bcd5bfac331011fd7230971448ac908f719
marjo1994 <m_rhl@live.com>
2025-08-20 09:11:46 -0500

    Fix(link): Fixing a link in Recipe Card

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -14,7 +14,7 @@ export const RecipeCard = ({recipe} : {recipe: Recipe}) => {
                 </Link>
                 <p>Ready in {recipe.readyInMinutes} minutes</p>
                 <p>Serves {recipe.servings}</p>
-                <Link to={`/recipe/${recipe.id}`} className="inline-flex items-center px-3 py-2 mt-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" target="_blank" rel="noopener noreferrer">
+                <Link to={`/recipe/${recipe.id}`} className="inline-flex items-center px-3 py-2 mt-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     View Recipe
                     <ArrowRightIcon className="ml-2 w-4 h-4" />
                 </Link>
